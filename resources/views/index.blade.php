d68d687f70d2d26a83fc75781b9a5b00a9c9f326
gudanggelo <taufik_14@apps.ipb.ac.id>
2019-05-13 19:47:42 +0700

    Update index.blade.php
    controller login di homepage diubah

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,9 +50,19 @@
 							</li>
 							<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
 						</ul>
-						<ul class="nav navbar-nav navbar-right">
-							<li class="nav-item"><a href="#" class="primary_btn text-uppercase">Login / SignUp</a></li>
-						</ul>
+						 @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
 					</div>
 				</div>
 			</nav>
