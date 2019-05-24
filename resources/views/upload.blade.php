3546d2731929e06fb432143bc2fd6402bf6d9050
gudanggelo <taufik_14@apps.ipb.ac.id>
2019-05-25 00:52:45 +0700

    form upload

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,5 +1,90 @@
 <!doctype html>
 <html lang="en">
+<style type="text/css">
+.form-style-1 {
+	margin:10px auto;
+	max-width: 400px;
+	padding: 20px 12px 10px 20px;
+	font: 13px "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+}
+.form-style-1 li {
+	padding: 0;
+	display: block;
+	list-style: none;
+	margin: 10px 0 0 0;
+}
+.form-style-1 label{
+	margin:0 0 3px 0;
+	padding:0px;
+	display:block;
+	font-weight: bold;
+}
+.form-style-1 input[type=text], 
+.form-style-1 input[type=date],
+.form-style-1 input[type=datetime],
+.form-style-1 input[type=number],
+.form-style-1 input[type=search],
+.form-style-1 input[type=time],
+.form-style-1 input[type=url],
+.form-style-1 input[type=email],
+textarea, 
+select{
+	box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	border:1px solid #BEBEBE;
+	padding: 7px;
+	margin:0px;
+	-webkit-transition: all 0.30s ease-in-out;
+	-moz-transition: all 0.30s ease-in-out;
+	-ms-transition: all 0.30s ease-in-out;
+	-o-transition: all 0.30s ease-in-out;
+	outline: none;	
+}
+.form-style-1 input[type=text]:focus, 
+.form-style-1 input[type=date]:focus,
+.form-style-1 input[type=datetime]:focus,
+.form-style-1 input[type=number]:focus,
+.form-style-1 input[type=search]:focus,
+.form-style-1 input[type=time]:focus,
+.form-style-1 input[type=url]:focus,
+.form-style-1 input[type=email]:focus,
+.form-style-1 textarea:focus, 
+.form-style-1 select:focus{
+	-moz-box-shadow: 0 0 8px #88D5E9;
+	-webkit-box-shadow: 0 0 8px #88D5E9;
+	box-shadow: 0 0 8px #88D5E9;
+	border: 1px solid #88D5E9;
+}
+.form-style-1 .field-divided{
+	width: 49%;
+}
+
+.form-style-1 .field-long{
+	width: 100%;
+}
+.form-style-1 .field-select{
+	width: 100%;
+}
+.form-style-1 .field-textarea{
+	height: 100px;
+}
+.form-style-1 input[type=submit], .form-style-1 input[type=button]{
+	background: #4B99AD;
+	padding: 8px 15px 8px 15px;
+	border: none;
+	color: #fff;
+}
+.form-style-1 input[type=submit]:hover, .form-style-1 input[type=button]:hover{
+	background: #4691A4;
+	box-shadow:none;
+	-moz-box-shadow:none;
+	-webkit-box-shadow:none;
+}
+.form-style-1 .required{
+	color:red;
+}
+</style>
     <head>
         <!-- Required meta tags -->
         <meta charset="utf-8">
@@ -77,7 +162,27 @@
         </section>
         <!--================End Home Banner Area =================-->
 <!-- form upload -->
-
+<form>
+<ul class="form-style-1">
+    <li><label>Nama<span class="required">*</span></label><input type="text" name="field1" class="field-divided" placeholder="Nama" /> </li>
+    <li>
+        <label>Email <span class="required">*</span></label>
+        <input type="email" name="field3" class="field-long" />
+    </li>
+    <li>
+        <label>Jenis Lembar Dokumen</label>
+        <select name="field4" class="field-select">
+        <option value="Advertise">A4</option>
+        <option value="Partnership">A3</option>
+        <option value="General Question">F4</option>
+        </select>
+    </li>
+    <li>
+        <label>Alamat <span class="required">*</span></label>
+        <textarea name="field5" id="field5" class="field-long field-textarea"></textarea>
+    </li>
+</ul>
+</form>
 	<div class="row">
 		<div class="container">
 			<div class="col-lg-8 mx-auto my-5">	
@@ -94,7 +199,7 @@
 					{{ csrf_field() }}
  
 					<div class="form-group">
-						<b>File Gambar</b><br/>
+						<b>File PDF WAJIB</b><br/>
 						<input type="file" name="file">
 					</div>
  
